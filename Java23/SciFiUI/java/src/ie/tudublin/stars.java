f60d1d8e2c7dd7210433dce7b5f88a4c75f39f1f
Java Software <pj.midgard@yahoo.com>
2019-05-17 11:55:42 +0100

    Add files via upload

--- Java23/SciFiUI/java/src/ie/tudublin/stars.java
+++ Java23/SciFiUI/java/src/ie/tudublin/stars.java
@@ -29,12 +29,97 @@ public class stars {
         ui.fill(255, 255, 255);
         ui.fill(255, 255, 0);
         ui.ellipse(len, lon, 7, 5);
+
+        ui.fill(len, lon, len);
+        ui.ellipse(310, lon, 5, 5);
+        ui.ellipse(320, lon, 5, 5);
+        ui.ellipse(330, lon, 5, 5);
+        ui.ellipse(330, lon, 5, 5);
+        ui.ellipse(340, lon, 5, 5);
+        ui.ellipse(350, lon, 5, 5);
+        ui.ellipse(360, lon, 5, 5);
+        ui.ellipse(370, lon, 5, 5);
+        ui.ellipse(380, lon, 5, 5);
+        ui.ellipse(390, lon, 5, 5);
+        ui.ellipse(400, lon, 5, 5);
+        ui.ellipse(410, lon, 5, 5);
+        ui.ellipse(420, lon, 5, 5);
+        ui.ellipse(430, lon, 5, 5);
+        ui.ellipse(440, lon, 5, 5);
+        ui.ellipse(450, lon, 5, 5);
+        ui.ellipse(460, lon, 5, 5);
+        ui.ellipse(470, lon, 5, 5);
+
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(350, lon, 5, 5);
+        ui.ellipse(360, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(380, lon, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(len, 510, 5, 5);
+        ui.ellipse(480, lon, 5, 5);
+
+        ui.ellipse(250, lon, 5, 5);
+        ui.ellipse(260, lon, 5, 5);
+        ui.ellipse(len, 430, 5, 5);
+        ui.ellipse(len, 440, 5, 5);
+        ui.ellipse(len, 450, 5, 5);
+        ui.ellipse(len, 460, 5, 5);
+        ui.ellipse(len, 470, 5, 5);
+        ui.ellipse(len, 480, 5, 5);
+        ui.ellipse(len, 490, 5, 5);
+        ui.ellipse(len, 400, 5, 5);
+        ui.ellipse(len, 410, 5, 5);
+        ui.ellipse(len, 420, 5, 5);
+        ui.ellipse(len, 430, 5, 5);
+        ui.ellipse(len, 440, 5, 5);
+        ui.ellipse(len, 450, 5, 5);
+        ui.ellipse(len, 460, 5, 5);
+        ui.ellipse(400, lon, 5, 5);
+        ui.ellipse(len, 480, 5, 5);
+        ui.ellipse(lon, 490, 5, 5);
+        ui.ellipse(len, 500, 5, 5);
+        ui.ellipse(lon, 410, 5, 5);
+        ui.ellipse(450, len, 5, 5);
+        ui.ellipse(lon, len, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+        ui.ellipse(len, lon, 5, 5);
+
         ui.noStroke();
 
-        ui.fill(255, 228, 128);
+        ui.fill(255, 248, 128);
         ui.rect(0, 0, 200, 800);
         ui.rect(700, 800, 200, -800);
         ui.rect(0, 100, 800, -800);
         ui.rect(0, 700, 800, 100);
+
     }
 }
